bc9329f8dd4173f6108091d174e331daa9066842
boseji <boseji@users.noreply.github.com>
2020-09-11 16:02:11 +0530

    Hashing passwords using BCrypt

--- main.go
+++ main.go
@@ -3,13 +3,36 @@ package main
 // Marshaling Example
 
 import (
-	"encoding/base64"
 	"fmt"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Print("\nHTTP Basic Authentication - base64\n\n")
-	authString := "user:pass"
-	enc := base64.StdEncoding.EncodeToString([]byte(authString))
-	fmt.Printf("\nBase64 Encoding for %q is %q\n\n", authString, enc)
+	fmt.Print("\nHashing Passwords - bcrypt\n\n")
+	pass := "123456789"
+	hash, err := hashPassword(pass)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Printf("Hashed password: %s\n\n", string(hash))
+	err = comparePassword(pass, hash)
+	if err != nil {
+		log.Fatalln("Not Logged In")
+	}
+	log.Println("Password Authentication Success!")
+}
+
+func hashPassword(password string) ([]byte, error) {
+	bs, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("Error in generating hash from password: \n %w", err)
+	}
+	return bs, err
+}
+
+func comparePassword(password string, hashedPassword []byte) error {
+	return bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
 }
