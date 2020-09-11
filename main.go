283ea3502f40ad3dc9b29063902a121e9823ee13
boseji <boseji@users.noreply.github.com>
2020-09-11 13:02:04 +0530

    Adding Server Structure for JSON Encode Decode Service

--- main.go
+++ main.go
@@ -3,9 +3,8 @@ package main
 // Marshaling Example
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 )
 
 type person struct {
@@ -13,33 +12,45 @@ type person struct {
 }
 
 func main() {
-	fmt.Println("\nJSON Un-Marshalling")
+	fmt.Println("\nJSON Server Setup")
 
-	p1 := person{
-		Name: "Devika",
-	}
+	// p1 := person{
+	// 	Name: "Devika",
+	// }
 
-	p2 := person{
-		Name: "Radhika",
-	}
+	// p2 := person{
+	// 	Name: "Radhika",
+	// }
 
-	xp := []person{
-		p1,
-		p2,
-	}
+	// xp := []person{
+	// 	p1,
+	// 	p2,
+	// }
 
-	bys, err := json.Marshal(xp)
-	if err != nil {
-		log.Panic(err)
-	}
+	// bys, err := json.Marshal(xp)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Printf("\nMarshalled: %q \n", string(bys))
+	// fmt.Printf("\nMarshalled: %q \n", string(bys))
 
-	xp2 := []person{}
-	err = json.Unmarshal(bys, &xp2)
-	if err != nil {
-		log.Panic(err)
-	}
+	// xp2 := []person{}
+	// err = json.Unmarshal(bys, &xp2)
+	// if err != nil {
+	// 	log.Panic(err)
+	// }
 
-	fmt.Printf("\nBack into Go Datastructure : %v\n\n", xp2)
+	// fmt.Printf("\nBack into Go Datastructure : %v\n\n", xp2)
+
+	http.HandleFunc("/encode", handleEncode)
+	http.HandleFunc("/decode", handleEncode)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleEncode(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleDecode(w http.ResponseWriter, r *http.Request) {
+
 }
