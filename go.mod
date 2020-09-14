32b106010728ce82fe96a9ed3043f0cb53531f2b
boseji <boseji@users.noreply.github.com>
2020-09-14 14:27:01 +0530

    Ninja Level 2 - Hands-On Exercise 1 - solution

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/boseji/golang-arch
 
 go 1.15
 
-require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/gofrs/uuid v3.3.0+incompatible // indirect
-)
+require golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
--- main.go
+++ main.go
@@ -1,165 +1,93 @@
 package main
 
-// Marshaling Example
-
 import (
-	"crypto/sha512"
-	"encoding/base64"
 	"fmt"
-	"io"
+	"html/template"
 	"log"
 	"net/http"
-	"time"
 
-	"github.com/dgrijalva/jwt-go"
-	"github.com/gofrs/uuid"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var mySigningKey = []byte("Super Secret long Password that works with 86546 number endings")
-
-type customClaims struct {
-	SessionID string `json:"session"`
-	Email     string `json:"email"`
-	jwt.StandardClaims
-}
-
-func getToken(session, email string, age time.Duration) (string, error) {
-	t := time.Now()
-	claims := customClaims{
-		SessionID: session,
-		Email:     email,
-		StandardClaims: jwt.StandardClaims{
-			Issuer:    "Test",
-			NotBefore: t.Unix(),
-			IssuedAt:  t.Unix(),
-			ExpiresAt: t.Add(age).Unix(),
-		},
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS512, &claims)
-	key := sha512.Sum512(mySigningKey)
-
-	ss, err := token.SignedString(key[:])
-	if err != nil {
-		return "", fmt.Errorf("couldn't get SignedString in NewWithClaims: %w", err)
-	}
-	return ss, nil
-}
-
-func checkToken(token string) (*customClaims, error) {
-
-	if token == "" {
-		return nil, fmt.Errorf("Empty Token")
-	}
-	tokenAfter, err := jwt.ParseWithClaims(token, &customClaims{},
-		func(tokenBefore *jwt.Token) (interface{}, error) {
-			if tokenBefore.Method.Alg() != jwt.SigningMethodHS512.Alg() {
-				return nil, fmt.Errorf("Wrong Signing method used")
-			}
-			key := sha512.Sum512(mySigningKey)
-			return key[:], nil
-		},
-	)
-	if err != nil {
-		return nil, fmt.Errorf("Error in checkToken while parsing token: %w", err)
-	}
-
-	if !tokenAfter.Valid {
-		return nil, fmt.Errorf("Error in checkToken since token is not Valid")
-	}
-
-	claims := tokenAfter.Claims.(*customClaims)
-
-	return claims, nil
-}
-
 func main() {
-	fmt.Print("\n JWT Cookie - with Verification \n\n")
+	fmt.Print("\nNinja Level 2 - Hands-On Exercise 1\n\n")
+	/*
+		Create a server with two endpoints
 
-	key := sha512.Sum512(mySigningKey)
-	fmt.Printf(" Our Key %q\n", base64.URLEncoding.EncodeToString(key[:]))
+			Endpoint / should display a webpage with a register form on it
+				This form should take in at least a username and password
+				It should post to /register
 
-	fmt.Println("\n Running Server on :8080")
+			Endpoint /register should save the username and password in a map
+				The password should be securely stored using bcrypt
+				Redirect the user back to endpoint / afterwards
+	*/
+	http.HandleFunc("/", root)
+	http.HandleFunc("/register", register)
 
-	http.HandleFunc("/", rootHandler)
-	http.HandleFunc("/submit", submitHandler)
-	http.ListenAndServe(":8080", nil)
+	fmt.Print("Starting Server on :8080\n\n")
+	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
 
-func submitHandler(w http.ResponseWriter, r *http.Request) {
+var store = map[string][]byte{}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	tpl := template.Must(template.ParseFiles("index.gohtml"))
+	tpl.Execute(w, nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		log.Printf("Bad Method : %v\n", r.Method)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	email := r.FormValue("emailField")
 	if email == "" {
+		log.Println("Email Empty")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	uid, err := uuid.NewV4()
-	if err != nil {
-		http.Error(w, "Couldn't session ID", http.StatusInternalServerError)
-		log.Println(err)
-		return
-	}
-	session := uid.String()
-
-	code, err := getToken(session, email, 5*time.Second)
-	if err != nil {
-		http.Error(w, "Couldn't get JWT", http.StatusInternalServerError)
-		log.Println(err)
+	pass := r.FormValue("passField")
+	if email == "" {
+		log.Println("Password Empty")
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
-	c := &http.Cookie{
-		Name:     "session",
-		Value:    code,
-		MaxAge:   5,
-		HttpOnly: true,
+	oldPass := []byte{}
+	if p, ok := store[email]; ok {
+		oldPass = p
 	}
 
-	http.SetCookie(w, c)
+	updated := len(oldPass) != 0
+
+	err := bcrypt.CompareHashAndPassword(oldPass, []byte(pass))
+	if err != nil && updated {
+		log.Println("Changing Passwords -", email)
+	}
+
+	olduser := updated && err == nil
+	updated = updated && err != nil
+
+	pw, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Failed to Login", http.StatusInternalServerError)
+		return
+	}
+
+	store[email] = pw
+
+	msg := "Created User -"
+	if updated {
+		msg = "Updated User -"
+	}
+	if olduser {
+		msg = "Valid User -"
+	}
+	log.Println(msg, email)
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	ck, err := r.Cookie("session")
-	if err != nil {
-		ck = &http.Cookie{}
-	}
-
-	valid := true
-	claims, err := checkToken(ck.Value)
-	if err != nil {
-		valid = false
-	}
-	html := `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>HMAC Cookie Example</title>
-	</head>
-	<body>
-		%s
-		<form action="/submit" method="post">
-			<input type="email" name="emailField">
-			<input type="submit">
-		</form>
-	</body>
-	</html>`
-
-	msg := "Not Logged In"
-	if valid {
-		msg = "Logged In as " + claims.Email
-	}
-	cookieAdd := `<p>Cookie Value: ` + ck.Value + `</p>
-				  <p>` + msg + `</p>`
-	if ck.Value == "" {
-		cookieAdd = ""
-	}
-
-	io.WriteString(w, fmt.Sprintf(html, cookieAdd))
 }
