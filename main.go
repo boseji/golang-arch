72c86d86f45ddc7aba92d82dde3eab37d7e25ed6
boseji <boseji@users.noreply.github.com>
2020-09-14 19:54:06 +0530

    Ninja Level 2 - Hands-On Exercise 2

--- main.go
+++ main.go
@@ -5,25 +5,29 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"net/url"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	fmt.Print("\nNinja Level 2 - Hands-On Exercise 1\n\n")
+	fmt.Print("\nNinja Level 2 - Hands-On Exercise 2\n\n")
 	/*
-		Create a server with two endpoints
-
-			Endpoint / should display a webpage with a register form on it
-				This form should take in at least a username and password
-				It should post to /register
-
-			Endpoint /register should save the username and password in a map
-				The password should be securely stored using bcrypt
-				Redirect the user back to endpoint / afterwards
+		For this hands-on exercise:
+			Modify the server from the previous exercise
+			Add a login form to the webpage
+				The form should take in a username and password
+				The form should post to a /login endpoint
+			Add a new endpoint /login
+				The endpoint should compare the given credentials with stored
+				credentials in the user map
+					Make sure to use the bcrypt function to compare the password
+			If the credentials match, display a webpage saying login successful
+			If the credentials do not match, display a webpage saying login failed
 	*/
-	http.HandleFunc("/", root)
+	http.HandleFunc("/", index)
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
 
 	fmt.Print("Starting Server on :8080\n\n")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
@@ -31,29 +35,42 @@ func main() {
 
 var store = map[string][]byte{}
 
-func root(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
+	errMsg := r.FormValue("errorMsg")
+	successMsg := r.FormValue("successMsg")
 	tpl := template.Must(template.ParseFiles("index.gohtml"))
-	tpl.Execute(w, nil)
+
+	message := ""
+	if errMsg != "" {
+		message = "Error - " + errMsg
+	}
+	if successMsg != "" {
+		message = successMsg + " - Success"
+	}
+	tpl.Execute(w, message)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		log.Printf("Bad Method : %v\n", r.Method)
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		log.Printf("Bad Method (register): %v\n", r.Method)
+		msg := url.QueryEscape("Bad Submit Method used")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
 		return
 	}
 
 	email := r.FormValue("emailField")
 	if email == "" {
 		log.Println("Email Empty")
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		msg := url.QueryEscape("Email needed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
 		return
 	}
 
 	pass := r.FormValue("passField")
-	if email == "" {
+	if pass == "" {
 		log.Println("Password Empty")
-		http.Redirect(w, r, "/", http.StatusSeeOther)
+		msg := url.QueryEscape("Password needed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
 		return
 	}
 
@@ -75,7 +92,8 @@ func register(w http.ResponseWriter, r *http.Request) {
 	pw, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
 	if err != nil {
 		log.Println(err)
-		http.Error(w, "Failed to Login", http.StatusInternalServerError)
+		msg := url.QueryEscape("Failed to Register due to Internal Server Error")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
 		return
 	}
 
@@ -89,5 +107,53 @@ func register(w http.ResponseWriter, r *http.Request) {
 		msg = "Valid User -"
 	}
 	log.Println(msg, email)
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+	msg = url.QueryEscape("Registered")
+	http.Redirect(w, r, "/?successMsg="+msg, http.StatusSeeOther)
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		log.Printf("Bad Method (login): %v\n", r.Method)
+		msg := url.QueryEscape("Bad Submit Method used")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	email := r.FormValue("emailField")
+	if email == "" {
+		log.Println("Email Empty (login)")
+		msg := url.QueryEscape("Email needed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	pass := r.FormValue("passField")
+	if pass == "" {
+		log.Println("Password Empty (login)")
+		msg := url.QueryEscape("Password needed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	oldPass := []byte{}
+	oldPass, ok := store[email]
+
+	err := bcrypt.CompareHashAndPassword(oldPass, []byte(pass))
+	if err != nil {
+		log.Println("Incorrect Passwords -", email)
+		msg := url.QueryEscape("Login Failed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	if !ok {
+		log.Println("User Does not Exist -", email)
+		msg := url.QueryEscape("Login Failed")
+		http.Redirect(w, r, "/?errorMsg="+msg, http.StatusSeeOther)
+		return
+	}
+
+	log.Println("Login Successful", email)
+	msg := url.QueryEscape("Logged In")
+	http.Redirect(w, r, "/?successMsg="+msg, http.StatusSeeOther)
 }
