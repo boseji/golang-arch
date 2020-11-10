8f6540e958b75bb793c386fd96a6df1f483dd67b
boseji <boseji@users.noreply.github.com>
2020-11-10 15:32:09 +0530

    Ninja Level 3 - Hands-On Exercise 3 - Heroku OAuth
    There is login fix applied for the API3 of Heroku
    Have a look at - "https://devcenter.heroku.com/articles/platform-api-reference#account-info"

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -24,52 +25,32 @@ var herokuOAuth2Config = &oauth2.Config{
 }
 
 // Key is UUID of login attempt , Value is expiration for loging attempt
-var loginAttempt = map[string]int64{}
+var loginAttempt = map[string]time.Time{}
 
 func main() {
-	fmt.Print("\nNinja Level 3 - Hands-On Exercise #2\n\n")
+	fmt.Print("\nNinja Level 3 - Hands-On Exercise #3\n\n")
 
 	/*
 		For this hands-on exercise:
 
-		- Modify the server from Ninja Level 2
-		- Create an oauth2.Config for your provider
-			> Fill it in with information from hand-on-exercise 1
-				= ClientID
-				= ClientSecret
-				= Endpoints
-					AuthURL
-					TokenURL
-				= RedirectURL
-					http://localhost:8080/oauth/<your provider>/receive
-				= Scopes
-					IF NEEDED
-		- Create an endpoint /oauth/<your provider>/login
-			> This should be a POST endpoint
-			> This should generate a uuid to use as the state value
-			> A new map should be used to save the state and the expiration time
-				or this login attempt
-				= Key is a string state
-				= Value is a time.Time expiration time
-					One hour in the future is a reasonable time
-			> Redirect the user to the oauth2 AuthCodeURL value
-		- Modify your / index page to include an oauth login form
-			> The form should post to /oauth/<your provider>/login
-			> The login form should only have a submit button
-		- LET’S TEST OUR CODE!
-			Verify that attempting to login sends you to the oauth provider’s
-			login page and approving sends you back to
-			/oauth/<your provider>/receive
-				You do not have this endpoint yet, if you are using the
-				http.ServeMux, your index page will serve this endpoint
-
+		- Modify the server from the previous exercise
+		- Create an endpoint /oauth/<your provider>/receive
+			> Should get the query parameter values state and code
+			> State should be used to see if this state has expired
+				= time.Now().After()
+			> Code should be sent through the config.Exchange function to get a token
+			> Create a TokenSource from the token
+			> Create an http.Client with the TokenSource
+			> Make your call you found in exercise 01 to get a user ID
+			> Print out the result from your call
 	*/
 
 	herokuOAuth2Config.ClientID = os.Getenv("CLIENT_ID")
 	herokuOAuth2Config.ClientSecret = os.Getenv("CLIENT_SECRET")
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/oauth/heroku/login", startHerokuOauth)
+	http.HandleFunc("/oauth/heroku/login", oHerokuLogin)
+	http.HandleFunc("/oauth/heroku/receive", oHerokuReceive)
 
 	log.Println("Starting Server on Port :8080")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
@@ -97,11 +78,11 @@ func attemptCode() (string, error) {
 		return "", fmt.Errorf("Error in attemptCode while generating UUID - %w", err)
 	}
 	uuid := u.String()
-	loginAttempt[uuid] = time.Now().Add(cLogingAttemptExpireDuration).Unix()
+	loginAttempt[uuid] = time.Now().Add(cLogingAttemptExpireDuration)
 	return uuid, nil
 }
 
-func startHerokuOauth(w http.ResponseWriter, r *http.Request) {
+func oHerokuLogin(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		log.Println("/oauth/heroku/login: Error Wrong Method invoked-", r.Method)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
@@ -117,4 +98,73 @@ func startHerokuOauth(w http.ResponseWriter, r *http.Request) {
 
 	redirectURL := herokuOAuth2Config.AuthCodeURL(uuid)
 	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
+
+func oHerokuReceive(w http.ResponseWriter, r *http.Request) {
+	code := r.FormValue("code")
+	state := r.FormValue("state")
+	if code == "" || state == "" {
+		log.Println("/oauth/heroku/receive: ERROR Invalid Request Received code-", code)
+		log.Println("/oauth/heroku/receive: state-", state)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	t, ok := loginAttempt[state]
+	if !ok {
+		log.Println("/oauth/heroku/receive: ERROR Invalid State received state-", state)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	delete(loginAttempt, state)
+
+	if time.Now().After(t) {
+		log.Println("/oauth/heroku/receive: ERROR Request Expired -", t.String())
+		http.Error(w, "Login request expired", http.StatusBadRequest)
+		return
+	}
+
+	tok, err := herokuOAuth2Config.Exchange(r.Context(), code)
+	if err != nil {
+		log.Println("/oauth/heroku/receive: Error in Oauth2 Exchange-", err)
+		http.Error(w, "Oops! Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+
+	ts := herokuOAuth2Config.TokenSource(r.Context(), tok)
+
+	client := oauth2.NewClient(r.Context(), ts)
+
+	// Fix for Non Standard version requirements in Header of GET request
+	//  - Heroku needs the header to be `Accept: application/vnd.heroku+json; version=3`
+	//  - However due to Golang http.Request automatic canonization the
+	//    'version' becomes "Version" which heroku API does not accept.
+	req, _ := http.NewRequest(http.MethodGet, "https://api.heroku.com/account", nil)
+	req.Header.Add("Accept", "application/vnd.heroku+json; version=3")
+	log.Println(req.Header)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("/oauth/heroku/receive: Error in Get Request via Client-", err)
+		http.Error(w, "Oops! Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	bs, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("/oauth/heroku/receive: Error in Reading response from Accounts request-", err)
+		http.Error(w, "Oops! Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+
+	if resp.StatusCode < http.StatusOK || resp.StatusCode > 299 {
+		log.Println("/oauth/heroku/receive: Error in Incorrect Response status code-", resp.StatusCode)
+		log.Println("/oauth/heroku/receive: Body-", string(bs))
+		http.Error(w, "Oops! Something went wrong.", http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Data returned : ", string(bs))
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
