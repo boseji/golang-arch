636c9bc9b04d027010a9453513aca7266cec9e98
boseji <boseji@users.noreply.github.com>
2021-02-10 10:36:20 +0530

    Unique Word count on STDIN

--- main.go
+++ main.go
@@ -4,29 +4,44 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
 
 // ---------------------------------------------------------
-// EXERCISE: Uppercaser
+// EXERCISE: Unique Words
 //
-//  Use a scanner to convert the lines to uppercase, and
-//  print them.
+//  Create a program that prints the total and unique words
+//  from an input stream.
 //
 //  1. Feed the swami_chinmayananda_geeta.txt to your program.
 //
 //  2. Scan the input using a new Scanner.
 //
-//  3. Print each line.
+//  3. Configure the scanner to scan for the words.
+//
+//  4. Count the unique words using a map.
+//
+//  5. Print the total and unique words.
+//
 //
 // EXPECTED OUTPUT
-//  Please run the solution to see the expected output.
+//
+//  There are 104 words, 74 of them are unique.
+//
 // ---------------------------------------------------------
 
 func main() {
+	totalWords := 0
+	wordcount := map[string]bool{}
 	// Create a Input Stream
 	in := bufio.NewScanner(os.Stdin)
+	// Word Splitter
+	in.Split(bufio.ScanWords)
+
 	for in.Scan() {
-		fmt.Printf("%s\n", strings.ToUpper(in.Text()))
+		totalWords++
+		word := in.Text()
+		wordcount[word] = true
 	}
+
+	fmt.Printf("There are %d words, %d of them are unique.\n", totalWords, len(wordcount))
 }
